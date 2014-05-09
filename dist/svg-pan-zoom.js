85651cca0bb391b9529d188a2ab63bad1d15311a
bumbu <bmbalex@gmail.com>
2014-05-09 18:55:27 +0200

    fix ie bug when an SVGElement is provided as selector

--- dist/svg-pan-zoom.js
+++ dist/svg-pan-zoom.js
@@ -951,7 +951,7 @@ module.exports = {
    */
 , isElement: function(o){
     return (
-      typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
+      typeof HTMLElement === "object" ? (o instanceof HTMLElement || o instanceof SVGElement || o instanceof SVGSVGElement) : //DOM2
       o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string"
     );
   }
