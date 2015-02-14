789552c17c90ba881ab5abb41242ac942cc34eac
bumbu <bmbalex@gmail.com>
2015-02-14 13:59:17 +0700

    Build and bump

--- dist/svg-pan-zoom.js
+++ dist/svg-pan-zoom.js
@@ -1,4 +1,4 @@
-// svg-pan-zoom v3.1.2
+// svg-pan-zoom v3.1.3
 // https://github.com/ariutta/svg-pan-zoom
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var svgPanZoom = require('./svg-pan-zoom.js');
@@ -20,156 +20,7 @@ var svgPanZoom = require('./svg-pan-zoom.js');
   }
 })(window, document)
 
-},{"./svg-pan-zoom.js":5}],2:[function(require,module,exports){
-(function(factory){
-  if ( typeof define === 'function' && define.amd ) {
-    // AMD. Register as an anonymous module.
-    define([], factory());
-  } else if (typeof exports === 'object') {
-    // Node/CommonJS
-    module.exports = factory();
-  } else {
-    // Browser globals
-    window.wheel = factory();
-  }
-}
-(function(){
-
-  //Full details: https://developer.mozilla.org/en-US/docs/Web/Reference/Events/wheel
-
-  var prefix = "", _addEventListener, _removeEventListener, onwheel, support, fns = [];
-
-  // detect event model
-  if ( window.addEventListener ) {
-    _addEventListener = "addEventListener";
-    _removeEventListener = "removeEventListener";
-  } else {
-    _addEventListener = "attachEvent";
-    _removeEventListener = "detachEvent";
-    prefix = "on";
-  }
-
-  // detect available wheel event
-  support = "onwheel" in document.createElement("div") ? "wheel" : // Modern browsers support "wheel"
-            document.onmousewheel !== undefined ? "mousewheel" : // Webkit and IE support at least "mousewheel"
-            "DOMMouseScroll"; // let's assume that remaining browsers are older Firefox
-
-
-  function createCallback(element,callback,capture) {
-
-    var fn = function(originalEvent) {
-
-      !originalEvent && ( originalEvent = window.event );
-
-      // create a normalized event object
-      var event = {
-        // keep a ref to the original event object
-        originalEvent: originalEvent,
-        target: originalEvent.target || originalEvent.srcElement,
-        type: "wheel",
-        deltaMode: originalEvent.type == "MozMousePixelScroll" ? 0 : 1,
-        deltaX: 0,
-        delatZ: 0,
-        preventDefault: function() {
-          originalEvent.preventDefault ?
-            originalEvent.preventDefault() :
-            originalEvent.returnValue = false;
-        }
-      };
-      
-      // calculate deltaY (and deltaX) according to the event
-      if ( support == "mousewheel" ) {
-        event.deltaY = - 1/40 * originalEvent.wheelDelta;
-        // Webkit also support wheelDeltaX
-        originalEvent.wheelDeltaX && ( event.deltaX = - 1/40 * originalEvent.wheelDeltaX );
-      } else {
-        event.deltaY = originalEvent.detail;
-      }
-
-      // it's time to fire the callback
-      return callback( event );
-
-    };
-
-    fns.push({
-      element: element,
-      fn: fn,
-      capture: capture
-    });
-
-    return fn;
-  }
-
-  function getCallback(element,capture) {
-    for (var i = 0; i < fns.length; i++) {
-      if (fns[i].element === element && fns[i].capture === capture) {
-        return fns[i].fn;
-      }
-    }
-    return function(){};
-  }
-
-  function removeCallback(element,capture) {
-    for (var i = 0; i < fns.length; i++) {
-      if (fns[i].element === element && fns[i].capture === capture) {
-        return fns.splice(i,1);
-      }
-    }
-  }
-
-  function _addWheelListener( elem, eventName, callback, useCapture ) {
-    
-    var cb;
-
-    if (support === "wheel") {
-      cb = callback;
-    } else {
-      cb = createCallback(elem,callback,useCapture);
-    }
-
-    elem[ _addEventListener ]( prefix + eventName, cb, useCapture || false );
-
-  }
-
-  function _removeWheelListener( elem, eventName, callback, useCapture ) {
-
-    if (support === "wheel") {
-      cb = callback;
-    } else {
-      cb = getCallback(elem,useCapture);
-    }
-
-    elem[ _removeEventListener ]( prefix + eventName, cb, useCapture || false );
-
-    removeCallback(elem,useCapture);
-
-  }
-
-  function addWheelListener( elem, callback, useCapture ) {
-    _addWheelListener( elem, support, callback, useCapture );
-
-    // handle MozMousePixelScroll in older Firefox
-    if( support == "DOMMouseScroll" ) {
-        _addWheelListener( elem, "MozMousePixelScroll", callback, useCapture);
-    }
-  }
-
-  function removeWheelListener(elem,callback,useCapture){
-    _removeWheelListener(elem,support,callback,useCapture);
-
-    // handle MozMousePixelScroll in older Firefox
-    if( support == "DOMMouseScroll" ) {
-        _removeWheelListener(elem, "MozMousePixelScroll", callback, useCapture);
-    }
-  }
-
-  return {
-    on: addWheelListener,
-    off: removeWheelListener
-  };
-
-}));
-},{}],3:[function(require,module,exports){
+},{"./svg-pan-zoom.js":4}],2:[function(require,module,exports){
 var SvgUtils = require('./svg-utilities');
 
 module.exports = {
@@ -293,7 +144,7 @@ module.exports = {
   }
 }
 
-},{"./svg-utilities":6}],4:[function(require,module,exports){
+},{"./svg-utilities":5}],3:[function(require,module,exports){
 var SvgUtils = require('./svg-utilities')
   , Utils = require('./utilities')
   ;
@@ -622,8 +473,8 @@ module.exports = function(viewport, options){
   return new ShadowViewport(viewport, options)
 }
 
-},{"./svg-utilities":6,"./utilities":7}],5:[function(require,module,exports){
-var Wheel = require('uniwheel')
+},{"./svg-utilities":5,"./utilities":7}],4:[function(require,module,exports){
+var Wheel = require('./uniwheel')
 , ControlIcons = require('./control-icons')
 , Utils = require('./utilities')
 , SvgUtils = require('./svg-utilities')
@@ -1348,7 +1199,7 @@ var svgPanZoom = function(elementOrSelector, options){
 
 module.exports = svgPanZoom;
 
-},{"./control-icons":3,"./shadow-viewport":4,"./svg-utilities":6,"./utilities":7,"uniwheel":2}],6:[function(require,module,exports){
+},{"./control-icons":2,"./shadow-viewport":3,"./svg-utilities":5,"./uniwheel":6,"./utilities":7}],5:[function(require,module,exports){
 var Utils = require('./utilities')
   , _browser = 'unknown'
   ;
@@ -1561,7 +1412,149 @@ module.exports = {
   }
 }
 
-},{"./utilities":7}],7:[function(require,module,exports){
+},{"./utilities":7}],6:[function(require,module,exports){
+// uniwheel 0.1.2 (customized)
+// A unified cross browser mouse wheel event handler
+// https://github.com/teemualap/uniwheel
+
+module.exports = (function(){
+
+  //Full details: https://developer.mozilla.org/en-US/docs/Web/Reference/Events/wheel
+
+  var prefix = "", _addEventListener, _removeEventListener, onwheel, support, fns = [];
+
+  // detect event model
+  if ( window.addEventListener ) {
+    _addEventListener = "addEventListener";
+    _removeEventListener = "removeEventListener";
+  } else {
+    _addEventListener = "attachEvent";
+    _removeEventListener = "detachEvent";
+    prefix = "on";
+  }
+
+  // detect available wheel event
+  support = "onwheel" in document.createElement("div") ? "wheel" : // Modern browsers support "wheel"
+            document.onmousewheel !== undefined ? "mousewheel" : // Webkit and IE support at least "mousewheel"
+            "DOMMouseScroll"; // let's assume that remaining browsers are older Firefox
+
+
+  function createCallback(element,callback,capture) {
+
+    var fn = function(originalEvent) {
+
+      !originalEvent && ( originalEvent = window.event );
+
+      // create a normalized event object
+      var event = {
+        // keep a ref to the original event object
+        originalEvent: originalEvent,
+        target: originalEvent.target || originalEvent.srcElement,
+        type: "wheel",
+        deltaMode: originalEvent.type == "MozMousePixelScroll" ? 0 : 1,
+        deltaX: 0,
+        delatZ: 0,
+        preventDefault: function() {
+          originalEvent.preventDefault ?
+            originalEvent.preventDefault() :
+            originalEvent.returnValue = false;
+        }
+      };
+
+      // calculate deltaY (and deltaX) according to the event
+      if ( support == "mousewheel" ) {
+        event.deltaY = - 1/40 * originalEvent.wheelDelta;
+        // Webkit also support wheelDeltaX
+        originalEvent.wheelDeltaX && ( event.deltaX = - 1/40 * originalEvent.wheelDeltaX );
+      } else {
+        event.deltaY = originalEvent.detail;
+      }
+
+      // it's time to fire the callback
+      return callback( event );
+
+    };
+
+    fns.push({
+      element: element,
+      fn: fn,
+      capture: capture
+    });
+
+    return fn;
+  }
+
+  function getCallback(element,capture) {
+    for (var i = 0; i < fns.length; i++) {
+      if (fns[i].element === element && fns[i].capture === capture) {
+        return fns[i].fn;
+      }
+    }
+    return function(){};
+  }
+
+  function removeCallback(element,capture) {
+    for (var i = 0; i < fns.length; i++) {
+      if (fns[i].element === element && fns[i].capture === capture) {
+        return fns.splice(i,1);
+      }
+    }
+  }
+
+  function _addWheelListener( elem, eventName, callback, useCapture ) {
+
+    var cb;
+
+    if (support === "wheel") {
+      cb = callback;
+    } else {
+      cb = createCallback(elem,callback,useCapture);
+    }
+
+    elem[ _addEventListener ]( prefix + eventName, cb, useCapture || false );
+
+  }
+
+  function _removeWheelListener( elem, eventName, callback, useCapture ) {
+
+    if (support === "wheel") {
+      cb = callback;
+    } else {
+      cb = getCallback(elem,useCapture);
+    }
+
+    elem[ _removeEventListener ]( prefix + eventName, cb, useCapture || false );
+
+    removeCallback(elem,useCapture);
+
+  }
+
+  function addWheelListener( elem, callback, useCapture ) {
+    _addWheelListener( elem, support, callback, useCapture );
+
+    // handle MozMousePixelScroll in older Firefox
+    if( support == "DOMMouseScroll" ) {
+        _addWheelListener( elem, "MozMousePixelScroll", callback, useCapture);
+    }
+  }
+
+  function removeWheelListener(elem,callback,useCapture){
+    _removeWheelListener(elem,support,callback,useCapture);
+
+    // handle MozMousePixelScroll in older Firefox
+    if( support == "DOMMouseScroll" ) {
+        _removeWheelListener(elem, "MozMousePixelScroll", callback, useCapture);
+    }
+  }
+
+  return {
+    on: addWheelListener,
+    off: removeWheelListener
+  };
+
+})();
+
+},{}],7:[function(require,module,exports){
 module.exports = {
   /**
    * Extends an object
