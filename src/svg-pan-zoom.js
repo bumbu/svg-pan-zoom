a8a3e69f374ac4688adedd6bbe50e9b3727bf1f9
bumbu <bmbalex@gmail.com>
2015-06-30 11:07:25 +0300

    Update default zoomScaleSensitivity to 0.1. Closes #126

--- src/svg-pan-zoom.js
+++ src/svg-pan-zoom.js
@@ -16,7 +16,7 @@ var optionsDefaults = {
 , dblClickZoomEnabled: true // enable or disable zooming by double clicking (default enabled)
 , mouseWheelZoomEnabled: true // enable or disable zooming by mouse wheel (default enabled)
 , preventMouseEventsDefault: true // enable or disable preventDefault for mouse events
-, zoomScaleSensitivity: 0.2 // Zoom sensitivity
+, zoomScaleSensitivity: 0.1 // Zoom sensitivity
 , minZoom: 0.5 // Minimum Zoom level
 , maxZoom: 10 // Maximum Zoom level
 , fit: true // enable or disable viewport fit in SVG (default true)
