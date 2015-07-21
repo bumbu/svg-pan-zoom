ec1c97cdff15b1b8b42f9e681604d777e6ef218b
bumbu <bmbalex@gmail.com>
2015-07-21 11:56:30 +0300

    Build and bump

--- dist/svg-pan-zoom.js
+++ dist/svg-pan-zoom.js
@@ -1,4 +1,4 @@
-// svg-pan-zoom v3.2.0
+// svg-pan-zoom v3.2.1
 // https://github.com/ariutta/svg-pan-zoom
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var svgPanZoom = require('./svg-pan-zoom.js');
@@ -189,7 +189,7 @@ ShadowViewport.prototype.cacheViewBox = function() {
   var svgViewBox = this.options.svg.getAttribute('viewBox')
 
   if (svgViewBox) {
-    var viewBoxValues = svgViewBox.split(' ').map(parseFloat)
+    var viewBoxValues = svgViewBox.split(/[\s\,]/).filter(function(v){return v}).map(parseFloat)
 
     // Cache viewbox x and y offset
     this.viewBox.x = viewBoxValues[0]
