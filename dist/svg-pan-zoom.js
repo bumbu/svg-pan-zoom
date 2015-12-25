092efbd95375073d8d48afc76f74827118f8c5ef
alex bumbu <bmbalex@gmail.com>
2015-12-25 14:01:52 +0000

    Build and bump

--- dist/svg-pan-zoom.js
+++ dist/svg-pan-zoom.js
@@ -1,4 +1,4 @@
-// svg-pan-zoom v3.2.5
+// svg-pan-zoom v3.2.6
 // https://github.com/ariutta/svg-pan-zoom
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var svgPanZoom = require('./svg-pan-zoom.js');
@@ -796,12 +796,13 @@ SvgPanZoom.prototype.publicZoomAtPoint = function(scale, point, absolute) {
     scale = this.computeFromRelativeZoom(scale)
   }
 
-  // If not a SVGPoint but has x and y than create a SVGPoint
-  if (Utils.getType(point) !== 'SVGPoint' && 'x' in point && 'y' in point) {
-    point = SvgUtils.createSVGPoint(this.svg, point.x, point.y)
-  } else {
-    throw new Error('Given point is invalid')
-    return
+  // If not a SVGPoint but has x and y then create a SVGPoint
+  if (Utils.getType(point) !== 'SVGPoint') {
+    if('x' in point && 'y' in point) {
+      point = SvgUtils.createSVGPoint(this.svg, point.x, point.y)
+    } else {
+      throw new Error('Given point is invalid')
+    }
   }
 
   this.zoomAtPoint(scale, point, absolute)
