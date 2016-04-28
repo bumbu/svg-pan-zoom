58b1c37cdc7952fb224bd6ed1759dc498e2e8179
alex bumbu <bmbalex@gmail.com>
2016-04-28 22:46:21 +0100

    Remove redundant line #195

--- src/svg-utilities.js
+++ src/svg-utilities.js
@@ -148,7 +148,6 @@ module.exports = {
     var that = this
       , s = 'matrix(' + matrix.a + ',' + matrix.b + ',' + matrix.c + ',' + matrix.d + ',' + matrix.e + ',' + matrix.f + ')';
 
-    element.setAttributeNS(null, 'transform', s);
     if ('transform' in element.style) {
       element.style.transform = s;
     } else if ('-ms-transform' in element.style) {
