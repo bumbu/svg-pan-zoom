c28dd61bc7cd3a35e4083946c593afe80c2f7684
alex bumbu <bmbalex@gmail.com>
2016-09-03 17:22:23 +0100

    Build and bump

--- dist/svg-pan-zoom.js
+++ dist/svg-pan-zoom.js
@@ -1,4 +1,4 @@
-// svg-pan-zoom v3.2.9
+// svg-pan-zoom v3.2.10
 // https://github.com/ariutta/svg-pan-zoom
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var svgPanZoom = require('./svg-pan-zoom.js');
@@ -586,7 +586,9 @@ SvgPanZoom.prototype.setupHandlers = function() {
   this.eventListeners = {
     // Mouse down group
     mousedown: function(evt) {
-      return that.handleMouseDown(evt, null);
+      var result = that.handleMouseDown(evt, prevEvt);
+      prevEvt = evt
+      return result;
     }
   , touchstart: function(evt) {
       var result = that.handleMouseDown(evt, prevEvt);
